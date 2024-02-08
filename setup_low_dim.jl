6941c0e6893d8a18294b3caa5a88b6028ec998eb
Alex de Beer <adeb970@aucklanduni.ac.nz>
2024-02-09 11:05:48 +1300

    Split setup scripts into individual scripts for high-dimensional and low-dimensional models

---
+++ setup_low_dim.jl
@@ -0,0 +1,102 @@
+using LinearAlgebra
+using Random: seed!
+
+include("EnsembleOED/EnsembleOED.jl")
+
+seed!(16)
+
+# ----------------
+# Discretisation, boundary conditions, forcing function
+# ----------------
+
+xmax = 6000
+nx = 61
+Δ = xmax/(nx-1)
+
+grid = Grid(nx, Δ)
+
+bcs = Dict(
+    :x0 => BoundaryCondition(:x0, :neumann, (x, y) -> 250 * 1e-3),
+    :x1 => BoundaryCondition(:x1, :neumann, (x, y) -> 0),
+    :y0 => BoundaryCondition(:y0, :dirichlet, (x, y) -> 100), 
+    :y1 => BoundaryCondition(:y1, :neumann, (x, y) -> 0)
+)
+
+function build_f(g::Grid)
+    f = zeros(g.nx^2)
+    for (i, c) ∈ enumerate(g.cs)
+        if i ∈ g.is_inner
+            if 4000 ≤ c[2] < 5000
+                f[i] = 0.137 * 1e-3
+            elseif 5000 ≤ c[2] ≤ 6000
+                f[i] = 0.274 * 1e-3
+            end
+        end
+    end
+    return f
+end
+
+f = build_f(grid)
+
+# ----------------
+# Prior
+# ----------------
+
+lnperm_int = 2.0
+lnperm_ext = 4.0
+
+# Bounds for slope and period of channel
+bnds_geom = [(-0.3, 0.3), (2000, 8000)]
+
+channel = ChannelGeom(grid, lnperm_int, lnperm_ext, bnds_geom)
+
+# ----------------
+# Measurement generation
+# ----------------
+
+n_data = 5
+
+# Candidate locations
+xs_cand = [1000] # LinRange(500, 5_500, 8)
+ys_cand = [5000] # LinRange(500, 5_500, 8)
+cs_cand = [(x, y) for y ∈ ys_cand for x ∈ xs_cand]
+
+M = length(cs_cand)
+
+B = generate_B(grid, cs_cand)
+
+F(u) = solve(grid, u, bcs, f)
+
+σ_ϵ = 0.02 * 150
+C_ϵ = σ_ϵ^2 * Matrix(1.0I, M, M)
+
+# ----------------
+# Testing
+# ----------------
+
+θ_t = rand(channel, 1)
+u_t = transform(channel, θ_t)
+F_t = F(u_t)
+G_t = B * F_t
+y = G_t + rand(MvNormal(C_ϵ))
+
+J = 100
+
+ens = Ensemble(channel, F, J)
+compute_Gs!(ens, B)
+
+run_eks!(ens, B, y, C_ϵ; Δt₀=0.5, γ=0.5, tmax=1.0)#; localised=false)
+
+n_grid = 100
+xis_grid = LinRange(-4, 4, n_grid)
+xjs_grid = LinRange(-4, 4, n_grid)
+zs_grid = zeros(n_grid, n_grid)
+
+for (i, xi) ∈ enumerate(xis_grid), (j, xj) ∈ enumerate(xjs_grid)
+
+    G_ij = B * F(transform(channel, [xi, xj]))
+    zs_grid[i, j] = 0.5norm([xi, xj])^2 + 0.5((G_ij - y)' * (C_ϵ \ (G_ij - y)))
+
+end
+
+compute_C_θθ(ens)
